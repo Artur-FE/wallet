23b87deb3d800d7a988a3bb4a9f2952e099e0d1f
Artur-FE <arturfedoseiev@gmail.com>
2025-07-10 21:37:19 +0200

    up Controller, create GlobalExceptionHandler

--- src/main/java/com/example/wallet/controller/WalletController.java
+++ src/main/java/com/example/wallet/controller/WalletController.java
@@ -1,22 +1,40 @@
 package com.example.wallet.controller;
 
+import com.example.wallet.dto.WalletRequestDto;
 import com.example.wallet.dto.WalletResponseDto;
 import com.example.wallet.service.WalletService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/wallet")
 @RequiredArgsConstructor
 
 public class WalletController {
 private final WalletService walletService;
+
     @PostMapping("/create")
     public WalletResponseDto create(){
         return walletService.createWallet();
+    }
+
+    @PostMapping
+    public WalletResponseDto operation(@Valid @RequestBody WalletRequestDto walletRequestDto){
+        WalletResponseDto operation = walletService.operation(walletRequestDto);
+        log.info("метод WalletResponseDto operation в контроллере отработал, баланс - {}", operation.getBalance());
+        return operation;
+    }
+
+    @GetMapping("/{WALLET_UUID}")
+    public WalletResponseDto getBalance(@PathVariable(value = "WALLET_UUID") UUID id){
+        log.info("метод getBalance контроллера отработал - ID: {}", id);
+      return walletService.getBalanceByWalletId(id);
     }
 
 }
